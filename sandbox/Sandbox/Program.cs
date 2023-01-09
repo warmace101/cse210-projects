6f584e2f78086ba191a7fc05fa55c29903b9ee34
Johnathan Babb <gamerj199@outlook.com>
2023-01-09 12:54:52 -0700

    Hopefully the second attempt works

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+        Console.WriteLine("Holy cow did I use this wrong before hahaha!");
     }
 }
