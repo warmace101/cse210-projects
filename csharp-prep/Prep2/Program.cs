ecafab5a515e51ed3e3bd5fac22a4a29ebb6137a
Johnathan Babb <gamerj199@outlook.com>
2023-01-10 20:05:35 -0700

    Completed Prep1 and Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePerc = Console.ReadLine();
+        int number = int.Parse(gradePerc);
+        if (number >= 90)
+        {
+            Console.WriteLine("You got and A! Congrats!!");
+        }
+        else if (number >= 80 && number <= 89)
+        {
+            Console.WriteLine("You got a B");
+        }
+        else if (number >= 70 && number <= 79)
+        {
+            Console.WriteLine("You got a C");
+        }
+        else if (number >= 60 && number<= 69)
+        {
+            Console.WriteLine("You got a D");
+        }
+        else if (number < 60)
+        {
+            Console.WriteLine("You failed!");
+        }
     }
 }
