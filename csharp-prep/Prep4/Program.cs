82b2e9b7ea884dcf0f765e51910d70ac6e683ab3
Johnathan Babb <gamerj199@outlook.com>
2023-01-18 12:55:25 -0700

    Prep 4 done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Eneter a number for the list.  Once you are done entering numbers please press 0.");
+        string userInput = Console.ReadLine();
+        int number = Convert.ToInt32(userInput);
+        int sum = number;
+        List<int> myNumb;
+        myNumb = new List<int>();
+        myNumb.Add(number);
+        while (number != 0)
+        {
+            Console.WriteLine("Enter another number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number != 0)
+            {
+                myNumb.Add(number);
+                sum = sum + number;
+
+            }
+
+        }
+    Console.WriteLine("The sum is " + sum.ToString());
+
+   if (sum != 0)//Removing divide by 0
+   { 
+        double avg = (double)sum / myNumb.Count;
+        Console.WriteLine($"The avereage is {avg}");
+   }
+    Console.WriteLine($"The max value is {myNumb.Max()}");
+    Console.WriteLine($"The count is {myNumb.Count}");
+    Console.WriteLine("Thanks for playing");
+
+
+
     }
 }
